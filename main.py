080ce6c3999c9dd7b6d6257b568cb6c7ac9a3d08
siruku@gmail.com <siruku@gmail.com>
2017-02-12 19:57:46 -0500

    Made numerous changes; added delete post handler; restructured class structure.

--- main.py
+++ main.py
@@ -20,51 +20,100 @@ import os
 import jinja2
 from google.appengine.ext import db
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+template_dir = os.path.join(os.path.dirname(__file__), 'build\\html')
 jinja_env = jinja2.Environment(loader= jinja2.FileSystemLoader(template_dir), autoescape= True)
+
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
 
 class Handler(webapp2.RequestHandler):
 
     def write(self, *a, **kwargs):
-        self.response.write(*a, **kwargs)
+        self.response.out.write(*a, **kwargs)
 
     def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
+        return render_str(template, **params)
 
     def render(self, template, **kwargs):
         self.write(self.render_str(template, **kwargs))
 
 
-class Art(db.Model):
-    title = db.StringProperty(required=True)
-    art = db.TextProperty(required=True)
-    created = db.DateTimeProperty(auto_now_add=True)
+def render_post(response, post):
+    response.out.write('<b>' + post.subject + '</b><br>')
+    response.out.write(post.content)
 
 
 class MainPage(Handler):
-
-    def render_front(self, title="", art="", error=""):
-        arts = db.GqlQuery("""SELECT * FROM Art ORDER BY created DESC""")
-        self.render("index.html", title=title, art=art, error=error, arts=arts)
-
     def get(self):
-        self.render_front()
+        self.redirect('/blog/')
+
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+
+class Post(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
+class BlogFront(Handler):
+    def get(self):
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        self.render('front.html', posts=posts)
+
+class PostPage(Handler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post=post)
+
+    def post(self, post_id):
+        delete = self.request.get('delete')
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if post and delete:
+            post.delete()
+            self.redirect_to(BlogFront)
+        else:
+            error = "Blah"
+            self.render("permalink.html", post=post, error=error)
+
+
+
+class NewPost(Handler):
+    def get(self):
+        self.render("newpost.html")
 
     def post(self):
-        title = self.request.get("title")
-        art = self.request.get("art")
+        subject = self.request.get('subject')
+        content = self.request.get('content')
 
-        if title and art:
-            a = Art(title=title, art=art)
-            a.put()
-
-            self.redirect("/")
+        if subject and content:
+            p = Post(parent=blog_key(), subject=subject, content=content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
         else:
-            error = "we need both a title and some artwork!"
-            self.render_front(title, art, error)
+            error = "subject and content please!"
+            self.render("newpost.html", subject=subject, content=content, error=error)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/blog/?', BlogFront),
+    ('/blog/([0-9]+)', PostPage),
+    ('/blog/newpost', NewPost)
 ], debug=True)
